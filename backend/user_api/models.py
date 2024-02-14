b0e3fb14e26333b7e738386d9c0a6bd13fca70f5
TheCheesyWiggle <fjlvanmontfort@gmail.com>
2024-02-14 16:44:44 +0000

    Created models for Question, Bag, Claim, Vendor, Extended User and added string representation functions

--- backend/user_api/models.py
+++ backend/user_api/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.base_user import BaseUserManager
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, User
 
 class AppUserManager(BaseUserManager):
 	def create_user(self, email, password=None):
@@ -28,8 +28,54 @@ class AppUser(AbstractBaseUser, PermissionsMixin):
 	user_id = models.AutoField(primary_key=True)
 	email = models.EmailField(max_length=50, unique=True)
 	username = models.CharField(max_length=50)
+	score = models.IntegerField(default=0)
 	USERNAME_FIELD = 'email'
 	REQUIRED_FIELDS = ['username']
 	objects = AppUserManager()
 	def __str__(self):
 		return self.username
+
+class WebsiteUserModel():
+	user_id = models.OneToOneField(AppUser, on_delete=models.CASCADE)
+	fname = models.CharField(max_length=25)
+	lname = models.CharField(max_length=25)
+	email = models.EmailField(max_length=25)
+	score = models.IntegerField(default=0)
+
+	def __str__(self):
+		return f'id:{self.user_id}, fname: {self.fname}, lname: {self.lname}, email: {self.email}, score: {self.score}'
+
+class VendorModel(models.Model):
+		vendor_id = models.AutoField(primary_key=True)
+		name = models.CharField(max_length=25)
+		num_bags = models.IntegerField()
+		location = models.CharField(max_length=25)
+
+		def __str__(self):
+				return f'vendor_id: {self.vendor_id}, name: {self.name}, num_bags: {self}, location: {self.location}'
+
+class BagModel(models.Model):
+		bag_id = models.AutoField(primary_key=True)
+		time = models.DateTimeField()
+		vendor_id = models.ForeignKey(VendorModel, on_delete=models.CASCADE)
+
+		def __str__(self):
+			return f'id: {self.bag_id}, time: {self}, vendor_id: {self.vendor_id}'
+
+class ClaimModel(models.Model):
+	claim_id = models.AutoField(primary_key=True)
+	bag_id = models.ForeignKey(BagModel, on_delete=models.CASCADE)
+	user_id = models.ForeignKey(AppUser, on_delete=models.CASCADE)
+	time = models.DateTimeField()
+	success = models.BooleanField(default=False)
+
+	def __str__(self):
+		return f'id: {self.claim_id}, bag_id: {self.bag_id}, user_id: {self}, time: {self.time}, success: {self}'
+
+class QuestionModel(models.Model):
+	question_id = models.AutoField(primary_key=True)
+	question = models.CharField(max_length=25)
+	answer = models.CharField(max_length=25)
+
+	def __str__(self):
+		return f'question_id: {self.question_id}, question: {self.question}, answer: {self.answer}'
