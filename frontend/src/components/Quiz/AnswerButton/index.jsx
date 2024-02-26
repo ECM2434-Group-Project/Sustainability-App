7b28edf2cebb448c664ba66c868c4f4968161006
inspizzz <wiktor.wiejak@gmail.com>
2024-02-26 13:10:17 +0000

    added button components to the components folder

--- frontend/src/components/Quiz/AnswerButton/index.jsx
+++ frontend/src/components/Quiz/AnswerButton/index.jsx
@@ -1,7 +1,7 @@
 export function AnswerButton({ answer, setSelected }) {
     return (
 		<div className="w-screen flex flex-col">
-			<button className="rounded-full p-4 justify-center m-1 border border-exeterDarkGreen hover:bg-exeterHighlightGreen ml-10 mr-10" onClick={
+			<button className="rounded-full p-4 justify-center m-1 border border-exeterDarkGreen hover:bg-exeterBrightGreen ml-10 mr-10" onClick={
 				() => {
 					setSelected(true)
 				}
--- frontend/src/components/Quiz/SubmitButton/index.jsx
+++ frontend/src/components/Quiz/SubmitButton/index.jsx
@@ -1,9 +1,11 @@
-export function SubmitButton({}) {
+import { Link } from "react-router-dom";
+
+export function  SubmitButton({ target }) {
     return (
 		<div className="w-screen flex flex-col mt-4">
-			<button className="rounded-full p-2 justify-center m-1 border mr-32 ml-32 hover:bg-red-500">
+			<Link className="rounded-full p-2 text-center m-1 border mr-32 ml-32 hover:bg-exeterHighlightGreen" to={target}>
 				Submit
-			</button>
+			</Link>
 		</div>
 		
     )
