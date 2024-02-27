480666396f6cf264a4f0b263a0a360e4b3372338
Arjun Modi <arjunm04@outlook.com>
2024-02-27 16:58:17 +0000

    Fixed backend error

--- backend/user_api/urls.py
+++ backend/user_api/urls.py
@@ -6,16 +6,11 @@ urlpatterns = [
 	path('login', views.UserLogin.as_view(), name='login'), # login a user
  	path('logout', views.UserLogout.as_view(), name='logout'), # logout a user
 	path('user', views.UserView.as_view(), name='user'), # see user info for logged in user
-
-
 	path('vendors', views.VendorsView.as_view(), name='vendors'), # see all vendors, low fidelity view
     path('vendors/<int:vendor_id>', views.VendorView.as_view(), name='specific-vendor-view'), # see a specific vendor
-
-
 	path('vendors/issuebags', views.IssueBagsView.as_view(), name='vendors'),
 	path('claims', views.ClaimsView.as_view(), name='claims'),
-	path('makeclaim', views.CreateClaim.as_view(), name='makeclaim')
-
+	path('makeclaim', views.CreateClaim.as_view(), name='makeclaim'),
 	path('questions', views.QuestionsView.as_view(), name='questions'),
 	path('leaderboard', views.LeaderboardView.as_view(), name='leaderboard'),
 	path('quiz', views.QuizView.as_view(), name='quiz'),
@@ -28,6 +23,4 @@ urlpatterns = [
 
 
 	path('geotest', views.GeoFenceTest.as_view(), name='testgeo')
-
-
-
+]
