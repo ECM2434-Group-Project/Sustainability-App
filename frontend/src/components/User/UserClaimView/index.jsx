b3d93c20b03af21d4ef16040a9cbaecb5b947e9c
Ed <edward.blewitt2@gmail.com>
2024-02-28 14:35:56 +0000

    Small styling fixes

--- frontend/src/components/User/UserClaimView/index.jsx
+++ frontend/src/components/User/UserClaimView/index.jsx
@@ -23,29 +23,33 @@ export function UserClaimView() {
 
 
     return !error ? (
-        claims.map(c => {
-            return (
-                <div className="flex gap-4 overflow-x-auto text-white w-full p-2 bg-gradient-to-r from-exeterDeepGreen to-exeterBrightGreen rounded-xl">
-                    <div className="bg-white p-[1.5px] rounded-xl">
-                        <div className="p-6 rounded-xl bg-gradient-to-r from-exeterDeepGreen to-exeterBrightGreen">
-                            <div className="rounded-xl bg-cover bg-center flex flex-col gap-4">
-    
-                                <div className="flex">
-                                    <h3 className="font-semibold text-6xl">
-                                        <TbPaperBag />
-                                    </h3>
+        <section className="flex flex-col gap-4">
+            {
+                claims.map(c => {
+                    return (
+                        <div className="flex gap-4 overflow-x-auto text-white w-full p-2 bg-gradient-to-r from-exeterDeepGreen to-exeterBrightGreen rounded-xl">
+                            <div className="bg-white p-[1.5px] rounded-xl flex-1 flex">
+                                <div className="p-6 rounded-xl bg-gradient-to-r from-exeterDeepGreen to-exeterBrightGreen flex-1 flex">
+                                    <div className="rounded-xl bg-cover bg-center flex flex-1 flex-col gap-4">
+            
+                                        <div className="flex">
+                                            <h3 className="font-semibold text-6xl">
+                                                <TbPaperBag />
+                                            </h3>
+                                        </div>
+            
+                                        <h4>You have a bag waiting</h4>
+            
+                                        <p>{c.expiration} Collect by this time</p>
+                                        
+                                    </div>
                                 </div>
-    
-                                <h4>You have a bag waiting</h4>
-    
-                                <p>{c.expiration} Collect by this time</p>
-                                
                             </div>
                         </div>
-                    </div>
-                </div>
-            )
-        })
+                    )
+                })
+            }
+        </section>
     ) : (
         <p className="text-red-300">An error occurred</p>    
     )
