0ee881d7dba7e37cc457759c6a78a55e52b6430b
inspizzz <wiktor.wiejak@gmail.com>
2024-03-01 16:37:22 +0000

    added more stuff to the claims page

--- frontend/src/screens/view-claim/index.jsx
+++ frontend/src/screens/view-claim/index.jsx
@@ -1,10 +1,20 @@
 
 import { UserClaimView } from "../../components/User/UserClaimView"
+import { UserAvatar } from "../../components/User/UserAvatar"
+import { GoBackLink } from "../../components/General/GoBackLink"
 
 export function ViewClaimPage() {
 
     return (
-        <section className="p-4">
+        <section className="p-6">
+
+            <div className="absolute top-2 left-2 z-10">
+				<GoBackLink href={"/"} />
+			</div>
+
+            <div className="flex justify-end">
+				<UserAvatar />
+			</div>
 
             <h1 className="text-4xl font-semibold pb-12">View your claims</h1>
 
