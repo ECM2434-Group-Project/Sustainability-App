18cbe65f4086a0746f8cb37e347d6a5bebf27af2
Arjun Modi <arjunm04@outlook.com>
2024-03-15 11:56:53 +0000

    Leaderboard API call

--- frontend/src/screens/leaderboard/leaderboard.jsx
+++ frontend/src/screens/leaderboard/leaderboard.jsx
@@ -1,20 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {GoBackLink} from "../../components/General/GoBackLink";
+import { useUser } from "../../contexts/userContext";
+import { client } from "../../axios";
+
 
 export default function LeaderboardPage() {
 
-    const [leaderboard, setLeaderboard] = React.useState([
-        {"name": "John Doe", "score": 100},
-        {"name": "Jane Doe", "score": 90},
-        {"name": "John Smith", "score": 80},
-        {"name": "Jane Smith", "score": 70},
-        {"name": "John Johnson", "score": 60},
-        {"name": "Jane Johnson", "score": 50},
-        {"name": "John Brown", "score": 40},
-        {"name": "Jane Brown", "score": 30},
-        {"name": "John White", "score": 20},
-        {"name": "Jane White", "score": 10},
-    ]);
+    useEffect(() => {
+        client.get("/api/leaderboard").then((response) => {
+            setLeaderboard(response.data);
+        })
+    }, []);
+
+    const [leaderboard, setLeaderboard] = React.useState([]);
 
     return (
         <section className="p-6">
@@ -34,7 +32,7 @@ export default function LeaderboardPage() {
                 {leaderboard.map((user, index) => (
                     <tr>
                     <td>{index + 1}</td>
-                    <td>{user.name}</td>
+                    <td>{user.username}</td>
                     <td>{user.score}</td>
                     </tr>
                 ))}
