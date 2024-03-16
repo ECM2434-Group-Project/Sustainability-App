1b18a04727f3cf977a7dd1bb9fc2d0dbf5ba92fa
Arjun Modi <arjunm04@outlook.com>
2024-03-16 12:51:39 +0000

    Correct QR generation

--- frontend/src/screens/change-username/index.jsx
+++ frontend/src/screens/change-username/index.jsx
@@ -1,18 +1,39 @@
 import {React, useEffect, useState} from "react";
 import { useCallback } from "react";
 import { GoBackLink } from "../../components/General/GoBackLink";
+import { useUser } from "../../contexts/userContext";
+import { client } from "../../axios";
 
 export default function ChangeUsername() {
 
-    const [username, setUsername] = useState("");
+  const user = useUser();
 
     const changeUsername = useCallback(() => {
-        setUsername(document.getElementById("username").value);
-    }, [setUsername]);
+        if (document.getElementById("username").value === "") {
+            alert("Username cannot be empty");
+            return;
+        }
 
-    useEffect(() => {
-        // Axios call to change username
-    }, [username])
+        if (document.getElementById("oldUsername").value === document.getElementById("username").value) {
+            alert("New username cannot be the same as the old username");
+            return;
+        }
+        
+        client.post("/api/user/updateuser", {
+            username: document.getElementById("oldUsername").value,
+            password: document.getElementById("password").value,
+            new_username: document.getElementById("username").value,
+        }).then((response) => {
+            if (response.status === 200) {
+                alert("Username changed successfully");
+            } else {
+                alert("Error changing username");
+            }
+        }
+        ).catch((error) => {
+            alert("Error changing username");
+        });
+        }, []);
 
   return (
     <section className="flex justify-center p-10">
@@ -22,9 +43,20 @@ export default function ChangeUsername() {
       <div className="w-full">
         <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
           <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="oldUsername">
+              Old Username
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="oldUsername"
+              type="text"
+              placeholder="Old Username"
+            />
+          </div>
+          <div className="mb-4">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
-              for="username"
+              htmlFor="username"
             >
               Username
             </label>
@@ -33,6 +65,20 @@ export default function ChangeUsername() {
               id="username"
               type="text"
               placeholder="Username"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="******************"
             />
           </div>
           <div className="flex items-center justify-between">
--- frontend/src/screens/delete-account/index.jsx
+++ frontend/src/screens/delete-account/index.jsx
@@ -1,14 +1,86 @@
 import { GoBackLink } from "../../components/General/GoBackLink";
+import { client } from "../../axios";
+import { useCallback, useState } from "react";
 
 export default function DeleteAccount() {
-    return (
-        <section className="p-4 flex flex-col gap-8">
-            <GoBackLink href={"/settings"} />
 
-            <div className="flex flex-col gap-4">
-                <h1>Are you sure you want to delete your account?</h1>
-                <button className="border-[1.2px] border-black p-4 rounded text-white bg-red-600">Delete</button>
-            </div>
-        </section>
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const deleteUser = useCallback(() => {
+        if (email === "") {
+            alert("Email cannot be empty");
+            return;
+        }
+
+        if (password === "") {
+            alert("Password cannot be empty");
+            return;
+        }
+
+        client.post("/api/user/deleteuser", {
+            email: email,
+            password: password,
+        }).then((response) => {
+            console.log(response.headers);
+            if (response.status === 200) {
+                alert("Account deleted successfully");
+            } else {
+                alert("Error deleting account");
+            }
+        }
+        ).catch((error) => {
+            console.log(error);
+            alert("Error deleting account");
+        });
+        }
+        , [email, password]);
+
+    return (
+        <section className="flex justify-center p-10">
+        <div className="absolute top-2 left-2 z-10">
+				<GoBackLink href={-1} />
+		</div>
+      <div className="w-full">
+      <h1>Are you sure you want to delete your account?</h1>
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={deleteUser}>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="text"
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="******************"
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div className="flex items-center justify-between">
+          <button className="border-[1.2px] border-black p-4 rounded text-white bg-red-600" type="submit">Delete</button>
+          </div>
+        </form>
+      </div>
+    </section>
     )
 }
