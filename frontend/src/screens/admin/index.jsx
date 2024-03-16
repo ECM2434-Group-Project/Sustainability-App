2ef77e1924ea713ccb10be6c6e1ac474832001ec
inspizzz <wiktor.wiejak@gmail.com>
2024-03-16 16:17:34 +0000

    started to implement backend to the admin

--- frontend/src/screens/admin/index.jsx
+++ frontend/src/screens/admin/index.jsx
@@ -3,6 +3,8 @@ import { UserAvatar } from "../../components/User/UserAvatar";
 import { VendorItem } from "../../components/Admin/VendorItem";
 import { Link, useNavigate } from "react-router-dom";
 import { Popup } from "../../components/General/Popup";
+import { useUser } from "../../contexts/userContext";
+import { NotAdmin } from "../../components/Admin/NotAdmin";
 
 export default function AdminPage() {
 
@@ -114,47 +116,62 @@ export default function AdminPage() {
 
 	const nav = useNavigate()
 
-	useEffect(() => {
+	const { user } = useUser()
 
+	useEffect(() => {
+		console.log(user)
 }, 	[])
 
-	return(
-		<section className="p-4 pl-36">
-			<Popup trigger={editing} setTrigger={setEditing} size="large">
-				<div className="bg-white p-4 rounded-lg">
-					content inside popup
-				</div>
-			</Popup>
+	return user ? (
+		<>
+			{
+				user.role === "ADMIN" ? (
+					<section className="p-4 pl-36">
+						<Popup trigger={editing} setTrigger={setEditing} size="large">
+							<div className="bg-white p-4 rounded-lg">
+								content inside popup
+							</div>
+						</Popup>
 
-			<div className="flex justify-between sticky top-0 bg-white pb-2 pt-2">
-				<div className="flex justify-between w-full pr-36">
-					<h1 className="font-extrabold text-4xl p-2 b-0 m-0">Welcome Admin</h1>
-					<button className="bg-exeterBlue pl-6 pr-6 pt-4 pb-4 rounded-lg" onClick={() => {
-						nav("/admin/create-vendor")
-					}}>
-						Create Vendor
-					</button>
-				</div>
-				
+						<div className="flex justify-between sticky top-0 bg-white pb-2 pt-2">
+							<div className="flex justify-between w-full pr-36">
+								<h1 className="font-extrabold text-4xl p-2 b-0 m-0">Welcome Admin</h1>
+								<button className="bg-exeterBlue pl-6 pr-6 pt-4 pb-4 rounded-lg" onClick={() => {
+									nav("/admin/create-vendor")
+								}}>
+									Create Vendor
+								</button>
+							</div>
+							
 
-				<UserAvatar />
-			</div>
+							<UserAvatar />
+						</div>
 
-			<div className="sticky top-10">
-				
-			</div>
+						<div className="sticky top-10">
+							
+						</div>
 
-			<div className="grid grid-cols-4 gap-20 pt-8 pr-48">
-				{
-					vendors.map((vendor, index) => {
-						return (
-							<VendorItem key={index} vendor={vendor} setEditing={setEditing}/>
-						)
-					})
-				}
-			</div>
+						<div className="grid grid-cols-4 gap-20 pt-8 pr-48">
+							{
+								vendors.map((vendor, index) => {
+									return (
+										<VendorItem key={index} vendor={vendor} setEditing={setEditing}/>
+									)
+								})
+							}
+						</div>
 
-			
-		</section>
+						
+					</section>
+				) : (
+					<NotAdmin />
+				)
+			}
+		</>
+		
+	) : (
+		<>
+			not logged in
+		</>
 	)
 }
