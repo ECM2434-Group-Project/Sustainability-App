25250198088f8e1f8f0bf819dfe94cad5321f3b2
Arjun Modi <arjunm04@outlook.com>
2024-03-16 16:56:42 +0000

    Update username works

--- frontend/src/screens/change-password/index.jsx
+++ frontend/src/screens/change-password/index.jsx
@@ -1,27 +1,37 @@
 import {React, useEffect, useState} from "react";
 import { useCallback } from "react";
 import { GoBackLink } from "../../components/General/GoBackLink";
+import { client } from "../../axios";
 
 export default function ChangePassword() {
 
-    const [password, setPassword] = useState("");
+    const [oldPassword, setOldPassword] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+    const [newConfirmPassword, setNewConfirmPassword] = useState("");
 
     const changePassword = useCallback(() => {
-        const inputPassword = (document.getElementById("password").value);
-        const inputConfirmPassword = (document.getElementById("confirmPassword").value);
-
-        if (inputPassword !== inputConfirmPassword) {
+        if (newPassword !== newConfirmPassword) {
             alert("Passwords do not match");
+        } else if (oldPassword === newPassword) {
+            alert("New password cannot be the same as old password");
+        } else if (newPassword === "") {
+            alert("Password cannot be empty");
         } else {
-            setPassword(inputPassword);
+            client.post("/api/user/updateuser", {
+                password: oldPassword,
+                new_password: newPassword,
+            }).then((response) => {
+                if (response.status === 200) {
+                    alert("Password changed successfully");
+                } else {
+                    alert("Error changing password");
+                }
+            }
+            ).catch((error) => {
+                alert("Error changing password");
+            });
         }
-    }, [setPassword]);
-
-
-
-    useEffect(() => {
-        // Axios call to change password
-    }, [password])
+    }, [oldPassword, newPassword, newConfirmPassword]);
 
   return (
     <section className="flex justify-center p-10">
@@ -30,6 +40,21 @@ export default function ChangePassword() {
 		</div>
       <div className="w-full">
         <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              for="oldPassword"
+            >
+              Old Password
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="oldPassword"
+              type="password"
+              placeholder="Old Password"
+              onChange={(e) => setOldPassword(e.target.value)}
+            />
+          </div>
           <div className="mb-4">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
@@ -42,6 +67,7 @@ export default function ChangePassword() {
               id="password"
               type="password"
               placeholder="Password"
+              onChange={(e) => setNewPassword(e.target.value)}
             />
           </div>
           <div className="mb-4">
@@ -56,6 +82,7 @@ export default function ChangePassword() {
               id="confirmPassword"
               type="password"
               placeholder="Password"
+              onChange={(e) => setNewConfirmPassword(e.target.value)}
             />
           </div>
           <div className="flex items-center justify-between">
