a7a2e86fa7bf710e2b4370724104b8beae50170d
inspizzz <wiktor.wiejak@gmail.com>
2024-03-17 22:49:50 +0000

    cleaned up some imports

--- frontend/src/components/General/GoBackLink/index.jsx
+++ frontend/src/components/General/GoBackLink/index.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom/dist";
 import { IoArrowBackOutline } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
 
@@ -6,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 export function GoBackLink() {
 
     let history = useNavigate();
-    
+
     return (
         <button className="h-10 w-10 flex justify-center items-center bg-white rounded-full shadow-md sticky top-4" onClick={() => {
             history(-1)
