73359febc5f87c6ead01f6ec4b45ded4cc0e85e3
Ed <edward.blewitt2@gmail.com>
2024-03-18 12:44:17 +0000

    Vendor admin features

--- frontend/src/components/VendorCategoriesView/CategoryView/index.jsx
+++ frontend/src/components/VendorCategoriesView/CategoryView/index.jsx
@@ -1,23 +1,43 @@
-import { TbPaperBag } from "react-icons/tb";
+import { TbPaperBag, TbX } from "react-icons/tb";
 
 
-export function CategoryView({ category }) {
+export function CategoryView({ category, edit }) {
     return (
         <div className="flex items-center gap-4 justify-between">
-            <div className="flex items-center gap-4">
-                <div className="text-red-700 text-5xl">
-                    <TbPaperBag />
+            <div className="flex items-center gap-2">
+
+                {
+                    edit ? (
+                        <button>
+                            <TbX />
+                        </button>
+                    ) : (
+                        <></>
+                    )
+                }
+
+                <div className="flex items-center gap-4">
+                    <div className="text-red-700 text-5xl">
+                        <TbPaperBag />
+                    </div>
+                    <h3>
+                        {category?.name}
+                    </h3>
                 </div>
-                <h3>
-                    {category?.name}
-                </h3>
+
             </div>
 
-            <div className="flex gap-2 items-center text-2xl">
-                <button>-</button>
-                <div className="px-1 bg-gray-200 text-lg">0{category?.quantity}</div>
-                <button>+</button>
-            </div>
+            {
+                !edit ? (
+                    <div className="flex gap-2 items-center text-2xl">
+                        <button>-</button>
+                        <div className="px-1 bg-gray-200 text-lg">0{category?.quantity}</div>
+                        <button>+</button>
+                    </div>
+                ) : (
+                    <></>
+                )
+            }
 
         </div>
     )
--- frontend/src/screens/leaderboard/leaderboard.jsx
+++ frontend/src/screens/leaderboard/leaderboard.jsx
@@ -1,45 +1,84 @@
 import React from "react";
 import {GoBackLink} from "../../components/General/GoBackLink";
 
+
+
 export default function LeaderboardPage() {
 
     const [leaderboard, setLeaderboard] = React.useState([
-        {"name": "John Doe", "score": 100},
-        {"name": "Jane Doe", "score": 90},
-        {"name": "John Smith", "score": 80},
-        {"name": "Jane Smith", "score": 70},
-        {"name": "John Johnson", "score": 60},
-        {"name": "Jane Johnson", "score": 50},
-        {"name": "John Brown", "score": 40},
-        {"name": "Jane Brown", "score": 30},
-        {"name": "John White", "score": 20},
-        {"name": "Jane White", "score": 10},
+        { first_name: "John", last_name: "Doe", "score": 100 },
+        { first_name: "Jane", last_name: "Doe", "score": 90 },
+        { first_name: "John", last_name: "Smith", "score": 80 },
+        { first_name: "Jane", last_name: "Smith", "score": 70 },
+        { first_name: "John", last_name: "Johnson", "score": 60 }
     ]);
 
     return (
-        <section className="p-6">
-            <div className="absolute top-2 left-2 z-10">
-				<GoBackLink href={"/settings"} />
-			</div>
-            <br></br>
-            <h1>Leaderboard</h1>
-        <div id="leaderboard">
-            <div class="ribbon"></div>
-            <table>
-                <tr>
-                    <th>Rank</th>
-                    <th>Name</th>
-                    <th>Score</th>
-                </tr>
-                {leaderboard.map((user, index) => (
-                    <tr>
-                    <td>{index + 1}</td>
-                    <td>{user.name}</td>
-                    <td>{user.score}</td>
-                    </tr>
-                ))}
-            </table>
-        </div>
+        <section className="p-6 flex flex-col gap-6">
+
+			<GoBackLink href={"/settings"} />
+
+            <h1 className="text-2xl font-semibold">Leaderboard</h1>
+
+            <div id="leaderboard">
+                <table className="w-full row-sp text-gray-800">
+                    <tbody>
+                        {
+                            leaderboard.map((user, i) => (
+                                <tr key={i} className="flex items-center py-2">
+                                    <td className="p-1 w-10">{i + 1}</td>
+                                    <td className="p-1 w-12 flex justify-center">
+                                        <div
+                                            className="w-10 h-10 flex justify-center items-center rounded-full bg-exeterDeepGreen"
+                                        >
+                                            <span
+                                                className="w-min h-min text-white font-semibold text-sm text-nowrap"
+                                            >{user.first_name[0] + user.last_name[0]}</span>
+                                        </div>
+                                    </td>
+                                    <td className="p-1 text-left" width={"100%"}>{user.first_name} {user.last_name}</td>
+                                    <td className="p-1">
+                                        <p className="text-md text-nowrap">
+                                            {user.score} <small className="text-gray-600">XP</small>
+                                        </p>
+                                    </td>
+                                </tr>
+                            ))
+                        }
+                        {
+                            // If you're not in the top 5
+                            <>
+                                <tr className="py-4">
+                                    <td colSpan={"100%"} className="text-center">....</td>
+                                </tr>
+
+                                <tr className="flex items-center py-2">
+                                    <td className="p-1 w-10">{42}</td>
+                                        <td className="p-1 w-12 flex justify-center">
+                                            <div
+                                                className="w-10 h-10 flex justify-center items-center bg-exeterBrightRed rounded-full"
+                                            >
+                                                <span
+                                                    className="w-min h-min text-white font-semibold text-sm text-nowrap"
+                                                >{"W"} {"W"}</span>
+                                            </div>
+                                        </td>
+                                        <td className="p-1 text-left" width={"100%"}>
+                                            <small className="text-gray-500">You</small>
+                                            <br />
+                                            {"Wiktor"} {"Wiejak"}
+                                        </td>
+                                        <td className="p-1">
+                                            <p className="text-md text-nowrap">
+                                                {420} <small className="text-gray-600">XP</small>
+                                            </p>
+                                        </td>
+                                </tr>
+                            </>
+                        }
+                    </tbody>
+                </table>
+            </div>
         </section>
     )
 }
