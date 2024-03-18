f4ea1088eedf2ff8eb7f133590de8ad9d514de42
Ed <edward.blewitt2@gmail.com>
2024-03-18 12:44:17 +0000

    Vendor admin features

--- frontend/src/screens/vendor-admin/scan/VendorScanPage.jsx
+++ frontend/src/screens/vendor-admin/scan/VendorScanPage.jsx
@@ -14,13 +14,21 @@ export function VendorScanPage() {
                 <GoBackLink href={"/vendor-admin"} />
             </div>
 
+            <div className="flex flex-col gap-2">
+                <h1 className="text-4xl font-semibold">Scan a code</h1>
 
-            <h1 className="text-4xl font-semibold">Scan a code</h1>
-
-            <input className="text-2xl p-2 bg-gray-200" placeholder="X X X X" type="text" onChange={e => setCode(e.target.value)} value={code} />
+                <input className="text-2xl p-2 bg-gray-200 w-full" placeholder="X X X X" type="text" onChange={e => setCode(e.target.value)} value={code} />
+            </div>
 
             <button
-                className="bg-exeterDimRed to-exeterBrightRed text-white flex gap-4 justify-center items-center p-4 rounded-2xl text-lg font-semibold"
+                disabled={code === ""}
+                className={
+                    code ? (
+                        "bg-exeterDimRed to-exeterBrightRed text-white flex gap-4 justify-center items-center p-4 rounded-2xl text-lg font-semibold"
+                    ) : (
+                        "bg-gray-400 to-gray-200 text-white flex gap-4 justify-center items-center p-4 rounded-2xl text-lg font-semibold"
+                    )
+                }
             >Check code</button>
             
         </section>
