b7254e0b73179acef5ad4acae80b1f6c89d433fd
inspizzz <wiktor.wiejak@gmail.com>
2024-03-20 14:22:29 +0000

    added error checking for logging in

--- frontend/src/contexts/userContext.js
+++ frontend/src/contexts/userContext.js
@@ -58,7 +58,7 @@ export const UserProvider = ({ children }) => {
 	
 		} catch (error) {
 			console.log(error);
-			return false
+			return error
 		}
 	}, [])
 
--- frontend/src/screens/login/Login.jsx
+++ frontend/src/screens/login/Login.jsx
@@ -30,26 +30,31 @@ export default function Login() {
 
 			// now login
 			const result = await login(email, password)
-			
+			console.log("the result is, ", result)
 			if (result === true) {
 				nav("/")
 			} else {
 				setError(result)
 			}
-
 		},
 		[error, email, password]
-	);
-
-	// useEffect(() => {
-	// 	refreshUser();
-	// }, []);
+	)
 
 	return !user ? (
 		<div className="h-screen flex flex-col justify-center p-4 bg-exeterDeepGreen text-white gap-6">
 			<form onSubmit={handleSubmit} className="flex flex-col gap-16">
 				<div className="flex flex-col gap-6">
 					<h1 className="text-2xl font-bold">Login</h1>
+
+					{
+						error ? (
+							<p className="text-center text-red-200">
+								Incorrect email or password
+							</p>
+						) : (
+							<></>
+						)
+					}
 
 					<TextInput
 						label={"Your Exeter Email"}
@@ -68,14 +73,6 @@ export default function Login() {
 						required
 					/>
 				</div>
-
-				{error ? (
-					<p className="p-4 text-center text-red-200">
-						Incorrect email or password
-					</p>
-				) : (
-					<></>
-				)}
 
 				<button
 					className="bg-exeterDarkGreen text-white flex gap-4 justify-center items-center p-4 rounded-2xl text-lg font-semibold pointer"
