f34a4cea48515f022077f9c8cf6f1ce94d30a12a
inspizzz <wiktor.wiejak@gmail.com>
2024-03-20 13:46:23 +0000

    stable admin image fetching

--- frontend/src/components/Admin/VendorItem/index.jsx
+++ frontend/src/components/Admin/VendorItem/index.jsx
@@ -1,9 +1,10 @@
 export function VendorItem({ vendor, setEditing }) {
 	return (
-		<div className="flex border rounded-3xl w-full box-content aspect-square hover:shadow-lg hover:border-black" onClick={() => {
+		<div className="relative flex border rounded-3xl w-full box-content aspect-square hover:shadow-lg hover:border-black" onClick={() => {
 			setEditing(vendor)
 		}}>
-			<img src={`http://127.0.0.1:8000/api/getvendorimage/${vendor.username}_icon.jpg`} alt={vendor.username} className="rounded-3xl" />
+			<h2 className="absolute flex w-full justify-center">{vendor.first_name}</h2>
+			<img src={vendor.icon} alt={vendor.username} className="rounded-3xl" />
 		</div>
 	)
 }
