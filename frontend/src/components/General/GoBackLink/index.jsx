03f61080f61b2b2bd76929f5a84dfabd2fb94c4a
Ed <edward.blewitt2@gmail.com>
2024-03-22 16:29:02 +0000

    GoBackLink now works properly in all cases

--- frontend/src/components/General/GoBackLink/index.jsx
+++ frontend/src/components/General/GoBackLink/index.jsx
@@ -2,13 +2,18 @@ import { IoArrowBackOutline } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
 
 
-export function GoBackLink() {
+export function GoBackLink({ href="" }) {
 
-    let history = useNavigate();
+    const history = useNavigate();
+    const nav = useNavigate()
 
     return (
         <button className="h-10 w-10 flex justify-center items-center bg-white rounded-full shadow-md sticky top-4" onClick={() => {
-            history(-1)
+            if(href === "") {
+                history(-1)
+            } else {
+                nav(href)
+            }
         }}>
             <IoArrowBackOutline />
         </button>
