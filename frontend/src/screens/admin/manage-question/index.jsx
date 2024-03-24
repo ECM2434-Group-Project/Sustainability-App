692939bd85caff5f893520c628d087e0e900dda1
Arjun Modi <arjunm04@outlook.com>
2024-03-24 16:38:35 +0000

    Background admin fixed

--- frontend/src/screens/admin/manage-question/index.jsx
+++ frontend/src/screens/admin/manage-question/index.jsx
@@ -189,7 +189,7 @@ export default function ManageQuestionPage() {
 		<>
 			{
 				user.role === "ADMIN" ? (
-					<div className="flex flex-col bg-exeterBlue p-4 h-dvh">
+					<div className="flex flex-col bg-exeterBlue p-4">
 
 						{/* creating questions */}
 						<Popup trigger={creating} setTrigger={setCreating} size="large">
