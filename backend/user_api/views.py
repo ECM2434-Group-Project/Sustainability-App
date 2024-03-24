0b06df5a28991a5102bf467afb3429e6ce49d3f4
Arjun Modi <arjunm04@outlook.com>
2024-03-24 13:36:38 +0000

    Removed debug

--- backend/user_api/views.py
+++ backend/user_api/views.py
@@ -1413,8 +1413,6 @@ def verify_email(request, token):
     emailModel = EmailVerification.objects.get(token=token)
     user = UserModel.objects.get(id = emailModel.user_id)
     user.email_verified = True
-    print("_____________________________")
-    print(f"Verifying {user.email}")
     user.save()
     email_verification.save()
     return HttpResponse("Email verified successfully.")
